aca65864a23e3021b4d13b9b830371c0b492af32
Bereket <faltamo.b@northeastern.edu>
2024-10-02 19:14:40 -0400

    second test case

--- process.cpp
+++ process.cpp
@@ -12,24 +12,37 @@
 #include <atomic>
 #include <algorithm>
 #include <map>
+#include <queue>
 
 #define PORT "3490"
 #define INITIAL_DELAY 5 // seconds
 #define BACKLOG 5
 
-struct PeerInfo {
-    std::string hostname;
-    int incoming_sockfd;
-    int outgoing_sockfd;
-};
-
 int own_id;
 int predecessor_id;
 int successor_id;
 int state = 0;
-std::map<int, PeerInfo> peers;
 const int TOKEN = 1;
 std::atomic<bool> has_token(false);
+
+struct Peer {
+    std::string hostname;
+    int incoming_sockfd;
+    int outgoing_sockfd;
+};
+std::map<int, Peer> peers;
+
+const int MARKER = 2;
+struct Snapshot {
+    int id;
+    bool active;
+    int state;
+    bool has_token;
+    std::map<int, bool> channel_recording;
+    std::map<int, std::vector<int>> channel_state;
+};
+
+Snapshot current_snapshot = {-1, false};
 
 std::vector<std::string> readHostsfile(const std::string& filename) {
     std::vector<std::string> hosts;
@@ -57,7 +70,7 @@ void configurePeers(std::vector<std::string> hosts) {
     predecessor_id = (own_id - 1 + hosts.size()) % hosts.size();
     successor_id = own_id % hosts.size() + 1;
 
-    std::cout << "{proc_id: " << own_id << ", state: " << state << ", predecessor: " << predecessor_id << ", successor: " << successor_id << "}" << std::endl;
+    std::cerr << "{proc_id: " << own_id << ", state: " << state << ", predecessor: " << predecessor_id << ", successor: " << successor_id << "}" << std::endl;
 }
 
 int initializeListener() {
@@ -170,17 +183,101 @@ void handleConnections(int server_sockfd) {
     }
 }
 
-void receiveToken() {
+// handles start snapshotting for all processes(including the initator)
+void startSnapshot(int snapshot_id, int marker_sender_id = -1){
+
+    // log snapshot started
+    std::cerr << "{proc_id: " << own_id  << ", snapshot_id: " << snapshot_id 
+              << ", snapshot: \"started\"" << std::endl;
+
+    // 1. record local state
+    current_snapshot = {snapshot_id, true, state, has_token.load(), {}, {}};
+
+    // 2. send markers on all outgoing channels
+    for (const auto& peer : peers){
+        std::cerr << "{proc_id: " << own_id << ", snapshot_id: " << snapshot_id 
+              << ", sender: " << own_id << ", receiver: " << peer.first 
+              << ", msg: \"marker\", state: " << state << ", has_token: " 
+              << (current_snapshot.has_token ? "YES" : "NO") << "}" << std::endl;
+
+        int message[2] = {MARKER, snapshot_id};
+        send(peers[peer.first].outgoing_sockfd, message, sizeof(message), 0);
+    }
+
+    // 3. start recording on all incoming channels(except for the channel the marker came on)
+    for(const auto& peer : peers){
+        if(peer.first == marker_sender_id)
+            current_snapshot.channel_recording[peer.first] = false;
+        else {
+            current_snapshot.channel_recording[peer.first] = true;
+            current_snapshot.channel_state[peer.first] = {};
+        }
+    }
+}
+
+void handleMarker(int sender_id, int snapshot_id, float marker_delay){
+    
+    if(!current_snapshot.active){ // / first time receiving the marker
+        std::this_thread::sleep_for(std::chrono::duration<float>(marker_delay));
+        startSnapshot(snapshot_id, sender_id);
+    } else {  // not the first time receiving the marker
+        // stop recording on channel C_ki
+        current_snapshot.channel_recording[sender_id] = false;
+
+        // log channel closed
+        std::cerr << "{proc_id: " << own_id << ":, snapshot_id: " << snapshot_id
+                  << ", snapshot: " << "channel closed, " 
+                  << "channel: " << sender_id << "-" << own_id
+                  << ", queue: [";
+        for(size_t i = 0; i < current_snapshot.channel_state[sender_id].size(); i++){
+            if(i > 0) std::cerr << ",";
+            std::cerr << current_snapshot.channel_state[sender_id][i];
+        }
+        std::cerr << "]}" << std::endl; 
+
+
+        // if all channels closed, finish the snapshot
+        auto isFalse = [](const auto& pair) { return !pair.second; };
+        if (std::all_of(current_snapshot.channel_recording.begin(), current_snapshot.channel_recording.end(), isFalse)) {
+            std::cerr << "{proc_id:" << own_id << ", snapshot_id: " << current_snapshot.id 
+                      << ", snapshot: \"finished\"}" << std::endl;
+            current_snapshot.active = false;
+        }
+    }
+}
+
+
+void processToken(float token_delay, int snapshot_state, int snapshot_id) {
+    while (true) {
+        if (has_token.load()) {
+            state++;
+            std::cerr << "{proc_id: " << own_id << ", state: " << state << "}" << std::endl;
+
+            if(state == snapshot_state)
+                startSnapshot(snapshot_id);
+            
+            std::this_thread::sleep_for(std::chrono::duration<float>(token_delay));
+            
+            std::cerr << "{proc_id: " << own_id << ", sender: " << own_id << ", receiver: " << successor_id << ", message: \"token\"" << "}" << std::endl;
+            int message[2] = {TOKEN, 0};
+            send(peers[successor_id].outgoing_sockfd, message, sizeof(message), 0);
+            
+            has_token.store(false);
+        }
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    }
+}
+
+void receiveMessages(float marker_delay) {
+    // beej's guid: ch. 7
     while (true) {
         fd_set readfds;
         FD_ZERO(&readfds);
         int maxFd = -1;
         
         for (const auto& pair : peers) {
-            // if(pair.first != own_id) {
-                FD_SET(pair.second.incoming_sockfd, &readfds);
-                maxFd = std::max(maxFd, pair.second.incoming_sockfd);
-            // }
+            FD_SET(pair.second.incoming_sockfd, &readfds);
+            maxFd = std::max(maxFd, pair.second.incoming_sockfd);
         }
         
         struct timeval tv;
@@ -194,60 +291,48 @@ void receiveToken() {
             continue;
         }
         
-        for (const auto& pair : peers) {
-            if (FD_ISSET(pair.second.incoming_sockfd, &readfds)) {
-                int recieved_token = 0;
-                int bytes_read = recv(pair.second.incoming_sockfd, &recieved_token, sizeof(int), 0);
-                if (bytes_read == sizeof(int) && recieved_token == TOKEN ) {
-                    std::cout << "{proc_id: " << own_id 
-                              << ", sender: " << pair.first 
-                              << ", receiver: " << own_id 
-                              << ", message:"  << recieved_token <<  "}" 
-                              << std::endl;
-                    has_token.store(true);
+        for (const auto& peer : peers) {
+            if (FD_ISSET(peer.second.incoming_sockfd, &readfds)) {
+                int message[2];
+                ssize_t bytes_read = recv(peer.second.incoming_sockfd, message, sizeof(message), 0);
+                if (bytes_read == sizeof(message)) {
+                    if(message[0] == TOKEN){
+                        std::cerr << "{proc_id: " << own_id << ", sender: " << peer.first 
+                                  << ", receiver: " << own_id << ", message: \"token\"" <<  "}"
+                                  << std::endl;
+                        if(current_snapshot.active && current_snapshot.channel_recording[peer.first]){
+                            current_snapshot.channel_state[peer.first].push_back(TOKEN);
+                        }
+                        has_token.store(true);
+                    } else if(message[0] = MARKER){
+                        handleMarker(peer.first, message[1], marker_delay);
+                    }
                 }
             }
         }
     }
 }
 
-void processToken(float t) {
-    while (true) {
-        if (has_token.load()) {
-            state++;
-            std::cout << "{proc_id: " << own_id << ", state: " << state << "}" << std::endl;
-            
-            std::this_thread::sleep_for(std::chrono::duration<float>(t));
-            
-            std::cout << "{proc_id: " << own_id << ", sender: " << own_id << ", receiver: " << successor_id << ", message: " << TOKEN << "}" << std::endl;
-            send(peers[successor_id].outgoing_sockfd, &TOKEN, sizeof(TOKEN), 0);
-            
-            has_token.store(false);
-        }
-        std::this_thread::sleep_for(std::chrono::milliseconds(100));
-    }
-}
-
 int main(int argc, char* argv[]) {
     // process cli arguments
-    if (argc != 5 && argc != 6) {
-        std::cerr << "Usage: " << argv[0] << " -h <hostfile> -t <sleep_time> [-x]" << std::endl;
-        return 1;
-    }
-
     std::string hostsfile;
-    double t;
-
+    float token_delay, marker_delay;
+    int snapshot_state = -1, snapshot_id;
     for (int i = 1; i < argc; i += 2) {
         std::string arg(argv[i]);
         if (arg == "-h")
             hostsfile = argv[i + 1];
         else if (arg == "-t")
-            t = std::stof(argv[i + 1]);
+            token_delay = std::stof(argv[i + 1]);
+        else if (arg == "-m")
+            marker_delay = std::stof(argv[i + 1]);
+        else if (arg == "-s")
+            snapshot_state = std::stoi(argv[i + 1]);
+        else if (arg == "-p")
+            snapshot_id = std::stoi(argv[i + 1]);
         else if (arg == "-x")
             has_token.store(true);
     }
-
 
     // Configure hosts and their ids
     std::vector<std::string> hosts = readHostsfile(hostsfile);
@@ -275,8 +360,8 @@ int main(int argc, char* argv[]) {
 
 
     // recieve and process token
-    std::thread receiveThread(receiveToken);
-    std::thread processThread(processToken, t);
+    std::thread receiveThread(receiveMessages, marker_delay);
+    std::thread processThread(processToken, token_delay, snapshot_state, snapshot_id);
 
     // join
     serverThread.join();
